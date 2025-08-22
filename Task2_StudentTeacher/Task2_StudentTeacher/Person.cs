9230039280d52993cc42527dd8c1ffece176a4b9
K Shirish Kumar <kulkarnishirish35@gmail.com>
2025-08-22 12:00:06 +0530

    Added Task2 Project

---
+++ Task2_StudentTeacher/Task2_StudentTeacher/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Task2_StudentTeacher
+{
+    //Base Person class
+    public class Person
+    {
+        public string Name { get; set; }
+        
+        //Constructor that sets the Name
+        public Person(string name)
+        {
+            Name = name;
+        }
+        //Override ToString to return the person's name
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Task2_StudentTeacher/Task2_StudentTeacher/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Task2_StudentTeacher
+{
+    //Teacher class inherits from Person
+    public class Teacher : Person
+    {
+        //Constructor that calls base constructor (Person)
+        public Teacher(string name) : base(name) { }
+        //Method to show that teacher is explaining
+        public void Explain()
+        {
+            Console.WriteLine($"{Name} is explaining");
+        }
+    }
+}
