3e1ffadb6bad26906c7fdaa06d4e11521835907d
K Shirish Kumar <kulkarnishirish35@gmail.com>
2025-08-26 16:11:00 +0530

    Added Task5_EvenNumbersArray Project

---
+++ Task5_EvenNumbersArray/Task5_EvenNumbersArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Task5_EvenNumbersArray
+{
+    static void Main()
+    {
+        //Declare Array to store 10 Integers
+        int[] numbers = new int[10];
+        Console.WriteLine("Enter 10 Integers:");
+        //Loop to take 10 integer inputs from user
+        for(int i = 0; i < 10; i++)
+        {
+            Console.Write($"Number {i + 1}:"); //Ask user for each number
+            numbers[i] = Convert.ToInt32(Console.ReadLine()); //Store input in array
+        }
+        Console.WriteLine("\n Even numbers are:");
+        //Loop through the array and check each number
+        foreach(int num in numbers)
+        {
+            //If the number is divisible by 2,its even
+            if(num %2 ==0)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
