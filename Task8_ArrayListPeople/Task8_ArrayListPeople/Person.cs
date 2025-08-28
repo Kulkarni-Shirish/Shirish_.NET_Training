d031c90c39aa89c995f8333cde3a25a6f6a1e3b9
K Shirish Kumar <kulkarnishirish35@gmail.com>
2025-08-28 13:03:09 +0530

    Added Task8_ArrayListPeople

---
+++ Task8_ArrayListPeople/Task8_ArrayListPeople/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+namespace PeopleArrayList
+{
+    //Person class 
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        //Constructor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        //Override ToString method to print details
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
---
+++ Task8_ArrayListPeople/Task8_ArrayListPeople/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+namespace PeopleArrayList
+{ 
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Create an ArrayList
+            ArrayList people = new ArrayList();
+            //Request info for 3 people
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Enter name of person {i}:");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Enter age of Person{i}:");
+                int age = int.Parse(Console.ReadLine());
+                //Add new Person object to ArrayList
+                people.Add(new Person(name, age));
+            }
+            Console.WriteLine("\n-- People in the list---");
+            //Print all people using foreach loop
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine("\nProgram Ended.");
+        }
+    }
+}
