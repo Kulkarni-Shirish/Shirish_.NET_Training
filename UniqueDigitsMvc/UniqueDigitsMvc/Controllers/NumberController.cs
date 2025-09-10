376318995ff12e9ce7ecfb4e125b331ff05540e5
K Shirish Kumar <kulkarnishirish35@gmail.com>
2025-09-10 17:56:11 +0530

    Added UniqueDigitsMvc Project

---
+++ UniqueDigitsMvc/UniqueDigitsMvc/Controllers/NumberController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniqueDigitsMvc.Models;
+using System.Linq;
+
+namespace UniqueDigitsMvc.Controllers
+{
+    public class NumberController : Controller
+    {
+        // GET: Display form
+        public IActionResult Index()
+        {
+            var model = new NumberViewModel();
+            return View(model);
+        }
+
+        // POST: Process input
+        [HttpPost]
+        public IActionResult Index(NumberViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Remove duplicate digits while preserving order
+                model.UniqueNumber = string.Concat(model.InputNumber.Distinct());
+            }
+            return View(model);
+        }
+    }
+}
---
+++ UniqueDigitsMvc/UniqueDigitsMvc/Models/NumberViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniqueDigitsMvc.Models
+{
+    public class NumberViewModel
+    {
+        [Required(ErrorMessage = "Please enter a 9-digit number")]
+        [RegularExpression(@"^\d{9}$", ErrorMessage = "Number must be exactly 9 digits")]
+        public string InputNumber { get; set; } = string.Empty; // User input
+
+        public string UniqueNumber { get; set; } = string.Empty; // Output
+    }
+}
