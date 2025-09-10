95f3b4da1c3ec8a30547b6bb0290595d69edf7cd
K Shirish Kumar <kulkarnishirish35@gmail.com>
2025-09-10 17:46:09 +0530

    Added FlowerSelectorMvc Project

---
+++ FlowerSelectorMvc/FlowerSelectorMvc/Controllers/FlowerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FlowerSelectorMvc.Models;
+
+namespace FlowerSelectorMvc.Controllers
+{
+    public class FlowerController : Controller
+    {
+        // GET: Display the form
+        public IActionResult Index()
+        {
+            var model = new FlowerViewModel();
+            return View(model);
+        }
+
+        // POST: Handle selection
+        [HttpPost]
+        public IActionResult Index(FlowerViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Display message in ViewBag
+            ViewBag.Message = $"You selected: {model.SelectedFlower}";
+            return View(model);
+        }
+    }
+}
---
+++ FlowerSelectorMvc/FlowerSelectorMvc/Models/FlowerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlowerSelectorMvc.Models
+{
+    public class FlowerViewModel
+    {
+        [Required(ErrorMessage = "Please select a flower")]
+        public string SelectedFlower { get; set; } = string.Empty; // Stores selected flower
+
+        public List<string> Flowers { get; set; } = new List<string>
+        {
+            "Rose",
+            "Tulip",
+            "Lily",
+            "Sunflower",
+            "Orchid",
+            "Daisy"
+        };
+    }
+}
